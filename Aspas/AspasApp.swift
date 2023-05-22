ce2c2e05104b4d946d27462a87539012fa3d09fd
Ashish Mathew <ashish.mathew@palnar.com>
2023-05-22 15:43:57 +0530

    Initial Commit

---
+++ Aspas/AspasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AspasApp.swift
+//  Aspas
+//
+//  Created by Palnar on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AspasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
